09a11d0a4d23c49fa03da84f9431fa9025e0fc04
Trần Trương Văn <vanttse170128@fpt.edu.vn>
2024-05-19 01:28:23 +0700

    add layout

--- TutorDemand.RazorWebApp/tailwind.config.js
+++ TutorDemand.RazorWebApp/tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./Pages/**/*.{cshtml,razor}",
-    "./Views/**/*.{cshtml,razor}",
-    "./wwwroot/js/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./Pages/**/*.{cshtml,razor}",
+        "./Views/**/*.{cshtml,razor}",
+        "./wwwroot/js/**/*.js",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#66a593",
+                secondary: "#f3969a",
+                success: "#56cc9d",
+                info: "#6cc3d5",
+                warning: "#ffce67",
+                danger: "#f43f5e"
+            }
+        },
+    },
+    plugins: [],
 };
