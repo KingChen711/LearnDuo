14791c892895d0e88eecfb3cdf5a21dd30f6e42b
LeXuanPhuoc-lab <stevenmarks147@gmail.com>
2024-05-25 14:09:39 +0700

    Adding Unit Of Work Pattern

--- TutorDemand.Business/Abstractions/ISubjectBusiness.cs
+++ TutorDemand.Business/Abstractions/ISubjectBusiness.cs
@@ -10,28 +10,30 @@ public interface ISubjectBusiness
 
     //  Summary:
     //      Synchronous methods 
-    public IBusinessResult Insert(Subject subject);
+    public IBusinessResult Create(Subject subject);
     public IBusinessResult Delete(Guid subjectId);
     public IBusinessResult Delete(int id);
     public IBusinessResult Update(Subject subject);
     public IBusinessResult GetById(Guid subjectId);
     public IBusinessResult GetById(int id);
     public IBusinessResult GetAll();
-    public IBusinessResult GetWithCondition(
-        Expression<Func<Subject, bool>> filter = null!,
-        Func<IQueryable<Subject>, IOrderedQueryable<Subject>> orderBy = null!,
-        string includeProperties = "");
+    //public IBusinessResult GetWithCondition(
+    //    Expression<Func<Subject, bool>> filter = null!,
+    //    Func<IQueryable<Subject>, IOrderedQueryable<Subject>> orderBy = null!,
+    //    string includeProperties = "");
 
     //  Summary:
     //      Asynchronous methods 
-    public Task<IBusinessResult> InsertAsync(Subject subject);
+    public Task<IBusinessResult> CreateAsync(Subject subject);
     public Task<IBusinessResult> DeleteAsync(Guid subjectId);
     public Task<IBusinessResult> UpdateAsync(Subject subject);
     public Task<IBusinessResult> GetByIdAsync(Guid subjectId);  
     public Task<IBusinessResult> GetAllAsync();
-    public Task<IBusinessResult> GetWithConditionAysnc(
-        Expression<Func<Subject, bool>> filter = null!,
-        Func<IQueryable<Subject>, IOrderedQueryable<Subject>> orderBy = null!,
-        string includeProperties = "");
+    //public Task<IBusinessResult> GetWithConditionAysnc(
+    //    Expression<Func<Subject, bool>> filter = null!,
+    //    Func<IQueryable<Subject>, IOrderedQueryable<Subject>> orderBy = null!,
+    //    string includeProperties = "");
 
+    public int SaveChanges();
+    public Task<int> SaveChangeAsync();
 }
