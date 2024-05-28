c029d76b985340a83bba5f46054aa6bfb8c5950f
Trần Trương Văn <vanttse170128@fpt.edu.vn>
2024-05-28 17:04:13 +0700

    refactor-to-repository-teaching-schedule

---
+++ TutorDemand.Data/Repositories/TeachingScheduleRepository.cs
@@ -0,0 +1,9 @@
+﻿using TutorDemand.Data.Base;
+using TutorDemand.Data.Entities;
+
+namespace TutorDemand.Data.Repositories;
+
+public class TeachingScheduleRepository: GenericRepository<TeachingSchedule>
+{
+    
+}
