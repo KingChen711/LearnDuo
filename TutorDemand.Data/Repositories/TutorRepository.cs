6f0247257bc45a9ec74780242a04ba54b9199957
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2024-06-08 16:04:12 +0700

    update repository

--- TutorDemand.Data/Repositories/TutorRepository.cs
+++ TutorDemand.Data/Repositories/TutorRepository.cs
@@ -5,7 +5,6 @@ namespace TutorDemand.Data.Repositories;
 
 public class TutorRepository : GenericRepository<Tutor>
 {
-    private readonly NET1704_221_5_TutorDemandContext _context;
     public TutorRepository()
     {
 
